4e6e769c2646ebdf1ebbedce3f113bc912502de7
Tranzorro <shiekadam@gmail.com>
2022-04-10 17:25:24 -0400

    Update arraystodo2.js

--- arraystodo2.js
+++ arraystodo2.js
@@ -10,13 +10,34 @@ function reverseMyArray(arr){
 console.log(reverseMyArray([1,2,3,4,5]))
 
 
-function rotateArray(){
-    return null;
+function rotateArray(arr, shiftByL, shiftByR){ // make array wrap from end to front of array, no built in funcs. allow negative shift. 
+    for (let i = 0; i< arr.length * shiftByR || i < arr.length * shiftByL; i++){
+        if(shiftByL){
+            arr[i] = arr[i + 1]; // take each element and set it to be whatever was ahead of itself (this shifts to the left.)
+            arr[arr.length] = arr[0]; // this is a left shift wrap of above value setter.
+        }
+        if(shiftByR){
+            arr[i] = arr[i - 1]; // take each element and set it to the value behind itself (this shifts to the right.)
+            arr[0] = arr[arr.length]; // take the end of the array and set index 0 to that value (this wraps end value back to start. this is a right shift operation)
+        }
+        // arr[arr.length] = arr[arr.length - 1]; // take the end of the arr and set it to be whatever was just behind it. likely not needed.
+    }
+    return arr;
 }
 
-function filterArray(){
-    return null;
+console.log(rotateArray([1,2,3,4,5],2))
+
+function filterArray(arr, min, max){ // given min and max range, retain only whats within range. no built in funcs.
+    let temp = [];
+    for(let i = 0; i< arr.length; i++){
+        if(i !== min || i !== max){ // if index is not min or max, set arr to itself.
+            temp[i] = arr[i];
+        }
+    }
+    arr = temp;
+    return arr;
 }
+console.log(filterArray([1,2,3,4,5,6,7,8,9,10],2,8))
 
 function concatArray(arr1, arr2){
     let theArray = [];
